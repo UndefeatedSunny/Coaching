039f48a94f86db01de2c004cef32e4302163432b
Sunny Chaudhary <51516961+UndefeatedSunny@users.noreply.github.com>
2020-10-13 14:33:18 +0530

    Create Delete_or_rename_a_file.c

---
+++ File Handling/Delete_or_rename_a_file.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int main()
+{
+
+ //   rename("C:\\Users\\SUNNY\\Desktop\\C Prorammin\\fprintf\\sunny1.txt","C:\\Users\\SUNNY\\Desktop\\C Prorammin\\fprintf\\munny.txt");
+ //   remove("C:\\Users\\SUNNY\\Desktop\\C Prorammin\\fprintf\\munny.txt");
+    return 0;
+}
