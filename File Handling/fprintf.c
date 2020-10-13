9d83b52d2eef608e841963cddc956963f9bb6725
Sunny Chaudhary <51516961+UndefeatedSunny@users.noreply.github.com>
2020-10-13 11:35:17 +0530

    Create fprintf.c

---
+++ File Handling/fprintf.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    FILE *fp;
+    int n1= 100;
+    char c[10]= "Sunny";
+    float n2= 11.121;
+
+    fp = fopen("sunny.txt","w");
+
+    if(fp==NULL)
+    {
+        printf("\nError: Cannot open File");
+        exit(1);
+    }
+    fprintf(fp,"%d\n",n1);
+    fprintf(fp,"%.3f\n",n2);
+    fprintf(fp,"%s",c);
+    printf("Task Done Successfully");
+    fclose(fp);
+}
+
+
+
+/*
+OUTPUT
+          100
+          11.121
+          Sunny
+*/
