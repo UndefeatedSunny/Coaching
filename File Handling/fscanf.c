5f8028a9d51ba6db88ab3cd4ae9c2aede0eff5f9
Sunny Chaudhary <51516961+UndefeatedSunny@users.noreply.github.com>
2020-10-13 11:53:45 +0530

    Create fscanf.c

---
+++ File Handling/fscanf.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int main()
+{
+    FILE *fp;
+    int n1;
+    float n2;
+    char c[10];
+
+    fp = fopen("sunny.txt","r");
+
+    if(fp==NULL)
+    {
+        printf("\nError: Cannot open File");
+        exit(1);
+    }
+    fscanf(fp,"%d",&n1);
+    fscanf(fp,"%f",&n2);
+    fscanf(fp,"%s",c);
+    printf("%d\n%f\n%s",n1,n2,c);
+    printf("\nTask Done Successfully");
+    fclose(fp);
+    return 0;
+}
+
+/*
+file
+            100 100.111 sunny
+
+output 
+            100
+            100.111000
+            sunny
+*/
