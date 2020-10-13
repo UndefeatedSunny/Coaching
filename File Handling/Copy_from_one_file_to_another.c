77790e69b7ccf7ae586325ed0f87d0da26083ac3
Sunny Chaudhary <51516961+UndefeatedSunny@users.noreply.github.com>
2020-10-13 13:30:13 +0530

    Create Copy_from_one_file_to_another.c

---
+++ File Handling/Copy_from_one_file_to_another.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int main()
+{
+    FILE *fp1, *fp2;
+    char ch;
+
+    fp1 = fopen("sunny.txt","r");    // Reading
+    fp2 = fopen("sunny1.txt","w");   // Writing
+
+    if(fp1==NULL || fp2==NULL)
+    {
+        printf("\nError: Cannot open File");
+        exit(1);
+    }
+
+    while((ch=fgetc(fp1))!=EOF)
+    {
+        fputc(ch,fp2);
+    }
+
+    fclose(fp1);
+    fclose(fp2);
+    return 0;
+}
